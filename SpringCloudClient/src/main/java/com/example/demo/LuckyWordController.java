5f868a73fb552712402fde3f608e9ae58cf092ee
Saurabh Prakash <saupraka@publicisgroupe.net>
2018-09-05 13:21:50 +0530

    adding changes for cloud bus

--- SpringCloudClient/src/main/java/com/example/demo/LuckyWordController.java
+++ SpringCloudClient/src/main/java/com/example/demo/LuckyWordController.java
@@ -1,16 +1,29 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+//@ConfigurationProperties for the cloud-bus
+@ConfigurationProperties(prefix = "country")
 public class LuckyWordController {
+	
+	//values for the cloud- config	
+	//@Value("${Capital}")
+	String capital;
 
-  @Value("${Capital}") String capital;
+	public String getCapital() {
+		return capital;
+	}
 
-  @GetMapping("/capitalCountry")
-  public String showLuckyWord() {
-    return "The capital of country is: "+ capital;
-  }
+	public void setCapital(String capital) {
+		this.capital = capital;
+	}
+
+	@GetMapping("/capitalCountry")
+	public String showLuckyWord() {
+		return "The capital of country is: " + capital;
+	}
 }
--- SpringCloudClient2/src/main/java/com/example/demo/FruitsController.java
+++ SpringCloudClient2/src/main/java/com/example/demo/FruitsController.java
@@ -1,16 +1,36 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@ConfigurationProperties(prefix = "eat")
 public class FruitsController {
 
-  @Value("${Fruits}") String fruits;
+	//@Value("${Fruits}")
+	String Fruits;
 
-  @GetMapping("/countryFruit")
-  public String showLuckyWord() {
-    return "The fruit of country is: "+ fruits;
-  }
+	/*public String getFruits() {
+		return fruits;
+	}
+
+	public void setFruits(String fruits) {
+		this.fruits = fruits;
+	}*/
+
+	@GetMapping("/countryFruit")
+	public String showLuckyWord() {
+		return "The fruit of country is >> : " + Fruits;
+	}
+
+	public String getFruits() {
+		return Fruits;
+	}
+
+	public void setFruits(String Fruits) {
+		this.Fruits = Fruits;
+	}
 }
